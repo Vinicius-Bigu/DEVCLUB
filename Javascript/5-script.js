61ec211850ae143549b39b711dbbeb411c0f5667
Vinicius Oliveira <umadbigu@gmail.com>
2026-03-31 14:54:36 -0300

    Fix variable assignment for input element

--- Javascript/5-script.js
+++ Javascript/5-script.js
@@ -12,7 +12,7 @@ querySelectorAll -> Trás todos os elementos que encontrar
 */
 
 const input = document.getElementsByName("nome-completo")[0]
-const elements = document.getElementsByClassName("paragraph-js")
+const input = document.getElementsByClassName("paragraph-js")
 
 console.log(input)
 console.log(elements)
