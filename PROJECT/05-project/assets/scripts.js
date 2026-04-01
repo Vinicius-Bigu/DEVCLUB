51b78ad89febbf0096e3e3145b1f0f52d84bd686
Vinicius Oliveira <umadbigu@gmail.com>
2026-04-01 20:51:03 -0300

    Adiciona arquivos HTML, CSS e JavaScript para conversão de moedas, incluindo melhorias na estrutura e estilo

---
+++ PROJECT/05-project/assets/scripts.js
@@ -0,0 +1,52 @@
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
+
+
+
+function trocarBandeira() {
+    const select = document.getElementById('moeda');
+    const imagem = document.getElementById('bandeira');
+
+    if (select.value === 'dolar') {
+        imagem.src = './../../../img/estados-unidos (1) 1.png';
+    } else if (select.value === 'euro') {
+        imagem.src = './../../../img/Design sem nome 3.png';
+    }
+}
+
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //valor em real
+    const currencyValueConverted = document.querySelector(".currency-value")
+    
+    const dolarToday = 5.2
+    const euroToday = 6.2
+
+    
+    if (currencySelect.value == "dolar"){
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(inputCurrencyValue / dolarToday)
+
+
+    }
+    if (currencySelect.value == "euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+        
+    }
+   
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+   
+
+}
+convertButton.addEventListener("click", convertValues)
+
