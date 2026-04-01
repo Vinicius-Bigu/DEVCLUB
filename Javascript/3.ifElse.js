202cf9a51ce4aab1aaf0e4797704a0ae0705b521
Vinicius-Bigu <umadbigu@gmail.com>
2026-04-01 10:35:35 -0300

    Adiciona arquivos de JavaScript, HTML e CSS para projetos diversos, incluindo variáveis, tipos de dados, controle de fluxo, funções e estrutura de página inicial.

---
+++ Javascript/3.ifElse.js
@@ -0,0 +1,21 @@
+/*  
+                <------------------ Controlador de fluxo ------------------>  
+    - If (se)
+    - Else (se não) 
+
+    Operadores de Comparação
+    > maior
+    < menor
+    >= maior ou igual
+    <= menor ou igual
+    == Igual
+*/
+
+const notaDoAluno = 4
+const notaDeCorte = 5
+
+if (notaDoAluno >= notaDeCorte) {
+    console.log('Aprovado')
+} else {
+    console.log('Reprovado')
+}
---
+++ Javascript/5-script.js
@@ -0,0 +1,18 @@
+/* 
+
+DOCUMENT -> HTML
+getElementById -> Trás um elemento pelo seu id
+getElementByClassName -> Trás todos os elementos com essa class
+getElementsByTagname -> Trás todos os elementos com essa tag
+getElementsByName -> Trás todos os elementos com esse name
+
+querrySelector -> Trás um elemento, o PRIMEIROque encontrar
+querySelectorAll -> Trás todos os elementos que encontrar
+
+*/
+
+const input = document.getElementsByName("nome-completo")[0]
+const elements = document.getElementsByClassName("paragraph-js")
+
+console.log(input)
+console.log(elements)
