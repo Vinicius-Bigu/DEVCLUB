930951d55d6075fa873bc9b2556cfec0589db055
Vinicius Oliveira <umadbigu@gmail.com>
2026-04-06 19:07:36 -0300

    Refactor code structure for improved readability and maintainability

---
+++ Javascript/AULAS/6-match.js
@@ -0,0 +1,40 @@
+/*
+
+            Match -> Matemática
+
+- pow (2,2) / potência / 2² = 4
+- sqrt (25,2) / raiz quadrada 
+- PI / 3.1415
+- Ceil (3.14) / Arredondar para cima
+- floor (3.14) / Arredondar para baixo
+- Random () / Aleatório
+
+
+            Operadores Aritméticos
+
++ -> Adição
+- -> Subtração
+* -> Multiplicação
+/ -> Divisão
+% -> Resto
+++ -> Incremento
+-- -> Decremento
+** -> Exponencial 
+    
+   
+            Operadores de Comparação
+
+== -> Igual //Compara o valor, mas não o tipo
+=== -> TOTALMENTE igual // compraa o VALOR e o TIPO
+
+!= -> Diferente // ele compara o valor, mas não o tipo
+!== -> TOTALMENTE diferente // Compara o VALOR e o TIPO
+
+> -> Maior
+< -> Menor
+>= -> Maior ou igual
+<= -> Menor ou igual
+
+
+*/
+
