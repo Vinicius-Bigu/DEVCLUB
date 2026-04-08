93bb7768cb67a9bcd5f78bfad1f065afbbda3af4
Vinicius Oliveira <umadbigu@gmail.com>
2026-04-08 12:21:43 -0300

    Remove outdated JavaScript files and HTML/CSS assets from previous projects; add new files for currency converter and random number generator projects with initial implementations.

--- Javascript/Parte-3/6-math.js
+++ Javascript/Parte-3/6-math.js
@@ -1,15 +1,30 @@
 /*
-
      <------------Match -> Matemática------------>
 
-- pow (2,2) / potência / 2² = 4
+- pow (2,2) / potência / 2² = 4 , 2³ = 8
 - sqrt (25,2) / raiz quadrada 
 - PI / 3.1415
 - Ceil (3.14) / Arredondar para cima
 - floor (3.14) / Arredondar para baixo
 - Random () / Aleatório
+*/
+
+/* const result = Math.pow(5,2) */
+
+/* const result = Math.sqrt(25) */
+
+/* const result = Math.PI */
+
+/* const result = Math.ceil(3.14) */
+
+/* const result = Math.floor(3.14) */
+
+/* const result = Math.random() */
+
+/* console.log(result) */
 
 
+/*
      <------------Operadores Aritméticos---------->
 
 + -> Adição
@@ -19,9 +34,10 @@
 % -> Resto
 ++ -> Incremento
 -- -> Decremento
-** -> Exponencial 
+** -> Exponencial
+*/
     
-   
+/*   
     <------------Operadores de Comparação----------->
 
 == -> Igual //Compara o valor, mas não o tipo
@@ -34,8 +50,9 @@
 < -> Menor
 >= -> Maior ou igual
 <= -> Menor ou igual
+*/
 
-
+/*
      <------------Operadores Lógicos------------>
 
 && -> AND -> E sempre que os dois operandos forem verdadeiros, o resultado será verdadeiro.
@@ -55,8 +72,9 @@
 typeof -> Retorna o tipo de dado
 
 delete -> Remove um item do objeto
+*/
 
-
+/*
 <------------------operadores ternários------------------>
 
 
