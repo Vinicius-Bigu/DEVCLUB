042998b29cafbc27b33a3cad04a3b4e653ce735d
Vinicius Oliveira <umadbigu@gmail.com>
2026-04-28 14:18:48 -0300

    Adiciona arquivos iniciais para o projeto de laços, incluindo For.js, index.html e script.js.

---
+++ Javascript/Parte-3/For.js
@@ -0,0 +1,17 @@
+/* 
+
+    -FOR
+      1) INICIALIZAÇÃO - Criar uma variável e colocar um valor inicial para ela.
+      2) CONDIÇÃO - Enquanto for TRUE, o laço coninuará iterando.
+                    Ele irá verificar antes de cada iteração;
+      3) EXPRESSÃO FINAL - O que irá ocorrer a cada vez que o nosso laço der uma volta.
+
+      for ([inicialização]; [condição]; [expressão final]){
+           Seu Código aqui
+      }
+
+*/
+
+for(let i = 0; i < 15; i++){
+    console.log(i)
+}
